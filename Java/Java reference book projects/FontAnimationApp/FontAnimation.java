1328fb8c9f3df24a3c26e4a3f9e44b6edb16c8e5
Anmoldeep1509 <anmoldeep1509@gmail.com>
2018-12-10 09:27:44 -0800

    Renovatio Out of order

--- Java/Java reference book projects/FontAnimationApp/FontAnimation.java
+++ Java/Java reference book projects/FontAnimationApp/FontAnimation.java
@@ -2,7 +2,95 @@ import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
 
+/*
+HTML code-
+<html>
+<head>
+<title> Applet Demonstrating Applet and Thread Programming in Java</title>
+</head>
+<body>
+<applet code=FontAnimation.class
+height = 800
+width = 400>
+</applet>
+</body>
+</html>
+*/
 public class FontAnimation extends Applet implements ActionListener
 {
-	
+    TextField text = new TextField(20);
+    TextField red = new TextField(20);
+    TextField gr = new TextField(20);
+    TextField blue = new TextField(20);
+    Button b1 = new Button("Apply");
+    String str = "Hello";
+    int size = 10;
+    Font f;
+    boolean inc = true;
+    int r, g, b;
+    Color fcolor;
+    
+    public void init()
+    {
+        add(new Label("Enter Text to animate here"));
+        add(text);
+        blue.setText("0");
+        add(new Label("Enter Value for Red Color here"));
+        add(red);
+        blue.setText("0");
+        add(new Label("Enter value for Green Color here"));
+        add(gr);
+        blue.setText("0");
+        add(new Label("Enter value for Blue Color here"));
+        add(blue);
+        blue.setText("0");
+        add(b1);
+        b1.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        if ((e.source)==b1) 
+        {
+            str = text.getText();
+            if(str == "")
+                str = "Hello";
+            r = Integer.parseInt(red.getText());
+            g = Integer.parseInt(gr.getText());
+            b = Integer.parseInt(blue.getText());
+            fcolor = new Color(r, g, b);
+            repaint();
+        }
+    }
+
+    public void paint(Graphics g)
+    {
+        f = new Font("Arial", Font.Bold, size);
+        g.setColor(fColor);
+        g.setFont(f);
+        g.drawString(str, 50, 200);
+
+        try
+        {
+            Thread.sleep(500);
+        }
+        catch (Exception e) 
+        {
+            //TODO: handle exception
+            System.out.println(e.getMessage);
+        }
+
+        if (inc == true) {
+            size += 10;
+            if(size == 100)
+                inc = false;
+        }
+        else
+        {
+            size -= 10;
+            if(size == 10)
+                inc = true;
+        }
+        repaint();
+    }
 }
