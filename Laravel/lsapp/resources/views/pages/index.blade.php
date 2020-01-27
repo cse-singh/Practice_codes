32b65a11babd9e5ff0a9d9ae66dca027012f5251
Anmoldeep1509 <anmoldeep1509@gmail.com>
2020-01-27 10:11:30 +0530

    initial commit 2.0

---
+++ Laravel/lsapp/resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    {{-- <h1>Welcome to Laravel</h1> --}}
+    <h1>{{$title}}</h1>
+    <p>This is the laravel application from the "Laravel from Scratch" Youtube series</p>    
+@endsection
+
